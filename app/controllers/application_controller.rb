ce55142c4fe6867f06df503bad853409d5dfbbc3
Mariel F <marielbeth@gmail.com>
2017-09-12 18:44:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,13 @@ class App < Sinatra::Base
   end
 
   post '/teams' do
+    @team = Team.new(params[:team])
 
+    params[:team][:members].each do |member_info|
+      Member.new(member_info)
+    end
+
+    @members = Member.all
 
     erb :teams
   end
